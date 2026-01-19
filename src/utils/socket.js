1764baaa1ac817d1e8ea47c5b66253d6b81d3f53
sahalsemeer <mohdsahalsemeer@icloud.com>
2026-01-19 17:03:13 +0530

    feature:websockets,message

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -8,9 +8,19 @@ const initSocket = (server) => {
   });
 
   io.on('connection',socket => {
-    socket.on('joinChat',() => {})
+    socket.on('joinChat',({userId,RecieveruserId}) => {
+      const room = [userId,RecieveruserId].sort().join('_')
+      socket.join(room)
 
-    socket.on('sendMessage',() => {})
+    })
+
+    socket.on('sendMessage',({name,userId,RecieveruserId,text}) => {
+      const room = [userId,RecieveruserId].sort().join('_')
+      console.log(room);
+      console.log(name+':'+text);
+      io.to(room).emit('message recieved:',{name,text})
+
+    })
 
     socket.on('disconnect',() => {})
 })
